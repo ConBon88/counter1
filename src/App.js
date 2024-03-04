551dd78034b0c49281c61307bfca43c7ecfd6104
Conor <conoroghosa.co@gmail.com>
2024-03-04 22:38:00 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    
+    const [important , setImportant] = React.useState(0)
+    
+    
+    function In(){
+    
+        setImportant(prevCount => prevCount + 1)
+        // made a callback function, instead of using "important" state. just good form
+    }
+    
+    function De(){
+    
+        setImportant(prevCount => prevCount - 1)
+        
+    }
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick = {De}>–</button>
+            <div className="counter--count">
+                <h1>{important}</h1>
+            </div>
+            <button className="counter--plus" onClick = {In}>+</button>
+        </div>
+    )
 }
-
-export default App;
